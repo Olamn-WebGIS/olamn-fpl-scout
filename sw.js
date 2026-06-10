17e9dcf9fb0fccf55c042bce4b874905e046cd16
Adeoye Favour <olamilekanfavour03@gmail.com>
2026-06-10 18:08:01 +0100

    Fix PWA setup: improve manifest, service worker, install button, and favicon links

--- sw.js
+++ sw.js
@@ -1,11 +1,54 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('fpl-scout-store').then((cache) => cache.addAll(['/']))
+const CACHE_NAME = 'fpl-scout-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).catch(() => {
+        // If cache fails, continue anyway
+      });
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((response) => {
+        // Clone the response
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      }).catch(() => {
+        // Return cached response if fetch fails
+        return caches.match('/index.html');
+      });
+    })
   );
 });
